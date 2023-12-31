8ec0aaadb302b442509d2d0f47a9728dbbc1cdcc
andrzej <awoloszkiewicz@icloud.com>
2023-12-31 12:55:35 +0100

    properties and item set for PROD

---
+++ Data Model/parishName.js
@@ -0,0 +1,70 @@
+export const data =
+    [
+        {
+            id: null,
+            type: 'property',
+            datatype: 'wikibase-item',
+            labels: {
+                pl: 'pod wezwaniem',
+                en: 'named after'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'string',
+            labels: {
+                pl: 'nazwa w dopełniaczu',
+                en: 'genetivus name'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'external-id',
+            labels: {
+                pl: 'id w geoecclesiae',
+                en: 'geoecclesiae id'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'quantity',
+            labels: {
+                pl: 'kolejność wezwania',
+                en: 'name after order number'
+            },
+            descriptions: {
+                pl: 'Dla wezwań złożonych istotne jest w jakiej kolejności są ułożone',
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'item',
+            labels: {
+                pl: 'Wezwanie',
+                en: null
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            },
+            claims: {
+                //type: Class
+                P4: 'Q95'
+            }
+        }]
