ea9d61b65fa763a69a5baf6fc54dcc7593e9d235
andrzej <awoloszkiewicz@icloud.com>
2024-01-04 12:31:39 +0100

    initial properties

---
+++ Data Model/mainProperties.js
@@ -0,0 +1,29 @@
+export const data =
+    [
+        {
+            id: null,
+            type: 'property',
+            datatype: 'wikibase-item',
+            labels: {
+                pl: 'jest to',
+                en: 'type'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'time',
+            labels: {
+                pl: 'data',
+                en: 'date'
+            },
+            descriptions: {
+                pl: null,
+                en: 'Date may be used to express temporal information at any level of granularity. Recommended best practice is to use an encoding scheme, such as the W3CDTF profile of ISO 8601 [W3CDTF].'
+            }
+        }
+    ]
