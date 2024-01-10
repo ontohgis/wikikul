35b93e9bd240ff3d8928be88001f7949f912c475
andrzej <awoloszkiewicz@icloud.com>
2024-01-10 15:34:03 +0100

    main properies and items for import parishes

--- Data Model/mainProperties.js
+++ Data Model/mainProperties.js
@@ -6,7 +6,46 @@ export const data =
             datatype: 'wikibase-item',
             labels: {
                 pl: 'jest to',
-                en: 'type'
+                en: 'instance of'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'wikibase-item',
+            labels: {
+                pl: 'miejscowość',
+                en: 'locality'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'wikibase-item',
+            labels: {
+                pl: 'dekanat',
+                en: 'deanery'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'wikibase-item',
+            labels: {
+                pl: 'diecezja',
+                en: 'diocese'
             },
             descriptions: {
                 pl: null,
@@ -24,6 +63,60 @@ export const data =
             descriptions: {
                 pl: null,
                 en: 'Date may be used to express temporal information at any level of granularity. Recommended best practice is to use an encoding scheme, such as the W3CDTF profile of ISO 8601 [W3CDTF].'
+            }
+        },
+        {
+            id: null,
+            type: 'item',
+            labels: {
+                pl: 'Parafia (Kościół katolicki ob. łacińskiego)',
+                en: 'Parish (Latin Church)',
+                de: 'Pfarre',
+                la: 'Parochia',
+                ru: 'парафия'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'item',
+            labels: {
+                pl: 'Właściwa miejscowość',
+                en: 'Proper dwelling',
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'item',
+            labels: {
+                pl: 'Dekanat (Kościół katolicki ob. łacińskiego',
+                en: 'Deanery (Latin Church)',
+                la: 'Decanatus'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'item',
+            labels: {
+                pl: 'Diecezja (Kościół katolicki ob. łacińskiego)',
+                en: 'Diocese (Latin Church)',
+                de: 'Diözese',
+                la: 'Dioecesis'
+            },
+            descriptions: {
+                pl: null,
+                en: null
             }
         }
     ]
