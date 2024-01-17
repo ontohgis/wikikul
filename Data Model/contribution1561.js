cf06c125106a8861bb822ff18c4ae463111b011a
andrzej <awoloszkiewicz@icloud.com>
2024-01-17 13:42:11 +0100

    base properties

---
+++ Data Model/contribution1561.js
@@ -0,0 +1,41 @@
+export const data =
+    [
+        {
+            id: null,
+            type: 'property',
+            datatype: 'wikibase-item',
+            labels: {
+                pl: 'płatnik',
+                en: 'payer'
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'wikibase-item',
+            labels: {
+                pl: 'wchodzi w skład',
+                en: null
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        },
+        {
+            id: null,
+            type: 'property',
+            datatype: 'wikibase-item',
+            labels: {
+                pl: 'znajduje się przy',
+                en: null
+            },
+            descriptions: {
+                pl: null,
+                en: null
+            }
+        }]
