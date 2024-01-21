caa14d03ed82d5a7bb12045e4862770f4a812185
bszady <historical.gis@gmail.com>
2024-01-21 23:39:21 +0100

    verification of 3 P and 1 Q

--- Data Model/contribution1561.js
+++ Data Model/contribution1561.js
@@ -5,50 +5,54 @@ export const data =
             type: 'property',
             datatype: 'wikibase-item',
             labels: {
-                pl: 'płatnik',
-                en: 'payer'
+                pl: 'płatnik podatku',
+                en: 'taxpayer'
             },
             descriptions: {
                 pl: null,
                 en: null
             }
+            //https://www.wikidata.org/wiki/Q1938414
         },
         {
             id: null,
             type: 'property',
             datatype: 'wikibase-item',
             labels: {
-                pl: 'wchodzi w skład',
-                en: 'is part of'
+                pl: 'część',
+                en: 'Is Part Of'
             },
             descriptions: {
                 pl: null,
                 en: null
             }
+            //http://purl.org/dc/terms/isPartOf; https://www.wikidata.org/wiki/Property:P361; http://www.cidoc-crm.org/cidoc-crm/P46i_forms_part_of
         },
         {
             id: null,
             type: 'property',
             datatype: 'wikibase-item',
             labels: {
-                pl: 'znajduje się przy',
-                en: 'works at'
+                pl: 'miejsce',
+                en: 'location'
             },
             descriptions: {
                 pl: null,
                 en: null
             }
+            //http://www.cidoc-crm.org/cidoc-crm/P53_has_former_or_current_location; https://www.wikidata.org/wiki/Property:P276
         },
         {
             id: null,
             type: 'item',
             labels: {
-                pl: 'Instytucja',
-                en: 'Institution'
+                pl: 'instytucja',
+                en: 'institution'
             },
             descriptions: {
                 pl: null,
                 en: null
             }
+            //https://www.diva-portal.org/smash/get/diva2:1368936/FULLTEXT01.pdf; https://www.wikidata.org/wiki/Q178706
         }
         ]
